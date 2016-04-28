4fcbf83db2d8f7e62857c8dc32e9176ff6d9b10b
Juven <juven1996@gmail.com>
2016-04-28 16:55:17 -0700

    i did stuffs ok?

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,1 +1,19 @@
+var sessionStarted = false;
 
+function getTime(){
+    var t = new Date();
+    document.getElementById("curTime").innerHTML= t.toString().substring(0,24);;
+}
+setInterval(getTime, 1000);
+
+function startTimer(){
+    var t = new Date();
+    var hours = t.getHours();
+    var minutes = t.getMinutes();
+    if(hours==16&&minutes==54&&sessionStarted == false){
+        document.getElementById("sessionStart").innerHTML+="hi";
+        sessionStarted = true;
+        clearInterval(sessStart);
+    }
+}
+var sessStart = setInterval(startTimer,1000);
